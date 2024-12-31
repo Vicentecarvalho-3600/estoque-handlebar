9264e9926c3dd7c8533344d0cb86256fd795ea03
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-12-31 00:35:25 -0300

    adicionado novas rotas no routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { ProductController } from "../controllers/ProductController.js";
 
 const router = Router();
+const productController = new ProductController();
+router.get("/products", (req, res) => productController.index(req, res));
+router.get("/products/new", (req, res) => res.render("products/new"));
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,16 +1,22 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import path from "path";
+import productRoutes from "./routes/routes.js";
 
 const app = express();
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-app.set("views", "./views");
+app.set("views", "./src/views");
 
+app.use(express.static(path.join(process.cwd(), "./public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(productRoutes);
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
