a8277b49379a952bfcef1d8d8fb5c753fc9ec76e
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-12-31 00:34:02 -0300

    adicionado ProductModel metodo findById

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -3,12 +3,25 @@ import db from "../config/database.js";
 export class ProductModel {
   async findAll() {
     try {
-      const products = db("produtos")
-        .select("products", "estoque.quantidade")
-        .leftJoin("estoque", "produtos.id", "estoque.id_produto");
+      const products = await db("produtos")
+        .select("produtos.*", "estoque.quantidade")
+        .leftJoin("estoque", "produtos.id", "estoque.produto_id");
       return products;
     } catch (error) {
       console.error("Erro ao buscar produtos", error);
+      throw error;
+    }
+  }
+
+  async findById(id) {
+    try {
+      const product = await db("produtos")
+        .select("produtos.*", "estoque.quantidade", "estoque.localizacao")
+        .leftJoin("estoque", "produtos.id", "estoque.produto_id")
+        .first();
+      return product;
+    } catch (error) {
+      console.error("Erro ao buscar produto", error);
       throw error;
     }
   }
