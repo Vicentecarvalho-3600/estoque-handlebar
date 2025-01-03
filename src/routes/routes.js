6398a5ee810dd5ad9b1578c98c0b61b2c29e5249
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2025-01-03 02:21:36 -0300

    adicionado routes novas rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,15 @@ const router = Router();
 const productController = new ProductController();
 router.get("/products", (req, res) => productController.index(req, res));
 router.get("/products/new", (req, res) => res.render("products/new"));
+router.get("/products/:id", (req, res) => productController.show(req, res));
+router.get("/products/:id/edit", (req, res) =>
+  productController.edit(req, res)
+);
 
 router.post("/products", (req, res) => productController.create(req, res));
+router.post("/products/:id", (req, res) => productController.update(req, res));
+router.post("/products/:id/del", (req, res) =>
+  productController.delete(req, res)
+);
 
 export default router;
